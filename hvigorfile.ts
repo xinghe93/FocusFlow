3c2c0aa40edb2a622043fcf890c63490cd50dd4c
xinghe <111168693+xinghe93@users.noreply.github.com>
2026-09-03 22:28:12 +0800

    feat: implement foundation

--- hvigorfile.ts
+++ hvigorfile.ts
@@ -1,6 +1,6 @@
 import { appTasks } from '@ohos/hvigor-ohos-plugin';
 
 export default {
-  system: appTasks, /* Built-in plugin of Hvigor. It cannot be modified. */
-  plugins: []       /* Custom plugin to extend the functionality of Hvigor. */
+  system: appTasks, /* Hvigor 内置插件，不可修改。 */
+  plugins: []       /* 用于扩展 Hvigor 功能的自定义插件。 */
 }
